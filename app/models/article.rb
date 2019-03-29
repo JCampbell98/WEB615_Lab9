a666425e6eb4c90816ce35a575715df89ef44517
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-29 08:45:30 -0400

    Update article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,12 +9,29 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer
+#  uuid       :string
+#  slug       :string
 #
 
 class Article < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :uuid, use: [:slugged, :finders]
+
+  before_create :generate_uuid
+  after_create :manually_update_slug
+
   has_many :comments
   belongs_to :user
 
   validates :title, presence: true
   validates :content, presence: true
+
+  def generate_uuid
+    self.uuid = "#{self.model_name.name}-" + SecureRandom.uuid
+  end
+
+  def manually_update_slug
+    self.update_column(:slug, self.uuid)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326014743) do
+ActiveRecord::Schema.define(version: 20190329120645) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180326014743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "uuid"
+    t.string "slug"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -28,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180326014743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "uuid"
+    t.string "slug"
   end
 
   create_table "users", force: :cascade do |t|
